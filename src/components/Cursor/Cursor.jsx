c84e5a0dc118d36e973b74bbb36120221e9783b3
ccrisolo <ccrisolo3@gmail.com>
2021-09-30 12:11:54 -0700

    resets layout for mobile first

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -6,3 +6,38 @@ const Cursor = () => {
 };
 
 export default Cursor;
+
+
+//functions for cursor below...
+//copy and paste in Root component and import Cursor component
+// const onMouseMove = e => {
+//     const cursor = document.querySelector(".cursor");
+//     cursor.style.left = `${e.pageX}px`;
+//     cursor.style.top = `${e.pageY}px`;
+// };
+
+// const onMouseDown = e => {
+//     const cursor = document.querySelector(".cursor");
+//     cursor.style.border = "2px solid grey";
+//     cursor.style.background = "grey";
+// };
+
+// const onMouseUp = e => {
+//     const cursor = document.querySelector(".cursor");
+//     cursor.style.border = "";
+//     cursor.style.background = "";
+// };
+// const onMouseLeave = e => {
+//     const cursor = document.querySelector(".cursor");
+//     cursor.style.display = "none";
+// };
+// const onMouseEnter = e => {
+//     const cursor = document.querySelector(".cursor");
+//     cursor.style.display = "";
+// };
+
+// window.addEventListener("mousemove", onMouseMove);
+// window.addEventListener("mousedown", onMouseDown);
+// window.addEventListener("mouseup", onMouseUp);
+// window.addEventListener("mouseleave", onMouseLeave);
+// window.addEventListener("mouseenter", onMouseEnter);
