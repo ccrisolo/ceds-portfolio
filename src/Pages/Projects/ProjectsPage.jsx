15a98f663c4665f2778a2687536941037281472b
ccrisolo <ccrisolo3@gmail.com>
2021-10-04 16:35:32 -0700

    saves progress

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LottieAnimation from "../../components/Lottie/LottieAnimation";
 import devGearsPurple from "../../animations/devGearsPurple.json"
+import './ProjectsPage.css'
 
 const ProjectsPage = () => {
     return (
         <>
-            <h2>This will be the projects section</h2>
+            <h2>My Work</h2>
             <div className='illustration-2'>
                 <LottieAnimation
                     lotti={devGearsPurple}
