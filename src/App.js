69bfb416ab3a6266d8ae87ce053cf2ce5d56442c
ccrisolo <ccrisolo3@gmail.com>
2021-10-05 12:54:25 -0700

    adds fade in function to projects page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 import LottieAnimation from "./components/Lottie/LottieAnimation";
 import Desk from "./animations/Desk.json";
 import devGearsPurple from "./animations/devGearsPurple.json";
@@ -7,6 +8,17 @@ import IntroPage from "./Pages/Intro/IntroPage";
 import ProjectsPage from "./Pages/Projects/ProjectsPage";
 
 function App() {
+    const [isVisible, setIsVisible] = useState(false);
+    const domRef = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setIsVisible(entry.isIntersecting));
+        });
+        observer.observe(domRef.current);
+        return () => observer.observe(domRef.current);
+    }, []);
+
     return (
         <>
             <div className='App'>
@@ -16,8 +28,13 @@ function App() {
                 <div className='intro-section'>
                     <IntroPage />
                 </div>
-                <div className='projects-section'>
-                   <ProjectsPage />
+                <div
+                    className={`projects-section ${
+                        isVisible ? `is-visible` : ""
+                    }`}
+                    ref={domRef}
+                >
+                    <ProjectsPage />
                 </div>
                 <div className='education-section'>
                     <h2>This will be the education section</h2>
