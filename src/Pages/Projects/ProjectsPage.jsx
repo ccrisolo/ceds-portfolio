3f7760f0bda7f73e473885d3d27b84672347689e
ccrisolo <ccrisolo3@gmail.com>
2021-10-04 17:04:39 -0700

    styles project container and project cards

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import LottieAnimation from "../../components/Lottie/LottieAnimation";
-import devGearsPurple from "../../animations/devGearsPurple.json"
-import './ProjectsPage.css'
+import devGearsPurple from "../../animations/devGearsPurple.json";
+import "./ProjectsPage.css";
 
 const ProjectsPage = () => {
     return (
         <>
-            <h2>My Work</h2>
             <div className='illustration-2'>
+                <h2>My Work</h2>
                 <LottieAnimation
                     lotti={devGearsPurple}
                     width={"100%"}
                     height={"100%"}
                 />
+            </div>
+            <div className='projects-container'>
+                <div className='project'>project 1</div>
+                <div className='project'>project 2</div>
+                <div className='project'>project 3</div>
+                <div className='project'>project 4</div>
             </div>
         </>
     );
