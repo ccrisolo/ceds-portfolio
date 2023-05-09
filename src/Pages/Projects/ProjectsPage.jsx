5ee91e3bf81a887750979fbbc1df34ccfc371515
ccrisolo <ccrisolo3@gmail.com>
2023-05-08 20:05:33 -0700

    update link to ecosystem mobile  project

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -64,7 +64,6 @@ const ProjectsPage = () => {
                                 }}
                                 target='#'
                                 href='https://drive.google.com/file/d/1fnRIRJV5QrOu4SFinu9qDoB5FS12zGyk/view?usp=share_link'
-                                // href='https://drive.google.com/file/d/1JddMP6KRXACQyprr2a4xnyuwAfRwm8Jh/preview'
                             >
                                 <FontAwesomeIcon icon={faVideo} color='white' />
                                 <span className='project-link-text'>
